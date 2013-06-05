e4a7517c91f657b745f47a1ffbe6a55dd7d31974
andre <andre@kubuntu>
2013-06-05 15:08:28 +0200

    messaggio del secondo commit

--- Determinant/src/determinant/Determinant.java
+++ Determinant/src/determinant/Determinant.java
@@ -14,6 +14,6 @@ public class Determinant {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int ciccio = 1;
     }
 }
