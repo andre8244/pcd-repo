16c3b1c0559b77c28631be568eeac01478d82548
andre <andre@kubuntu>
2013-06-18 19:31:22 +0200

    *Commit Message*

--- DeterminantCalculatorService/src/java/determinant_calculator_service/MatrixUtil.java
+++ DeterminantCalculatorService/src/java/determinant_calculator_service/MatrixUtil.java
@@ -3,7 +3,6 @@ package determinant_calculator_service;
 import log.l;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
