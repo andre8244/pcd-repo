ea8fa7ceccbda2729a24c6e4a09fcbf7741e3963
andre <andre@kubuntu>
2013-06-18 17:44:25 +0200

    *Commit Message*

--- DeterminantCalculatorService/src/java/determinant_calculator_service/DeterminantCalculatorService.java
+++ DeterminantCalculatorService/src/java/determinant_calculator_service/DeterminantCalculatorService.java
@@ -1,5 +1,6 @@
 package determinant_calculator_service;
 
+import java.net.URL;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -14,9 +15,9 @@ public class DeterminantCalculatorService {
 	/**
 	 * Operazione per calcolare il determinante
 	 */
-	@WebMethod(operationName = "computeDeterminant")
-	public String computeDeterminant(@WebParam(name = "order") int order, @WebParam(name = "fileValues") String fileValues) {
-		return DeterminantCalculatorManager.getInstance().computeDeterminant(order, fileValues);
+	@WebMethod(operationName = "computeDeterminant") // TODO sistemare il tipo di fileValues
+	public String computeDeterminant(@WebParam(name = "order") int order, @WebParam(name = "fileValues") URL fileValues) {
+		return DeterminantCalculatorManager.getInstance().computeDeterminant(order, fileValues.toString());
 	}
 
 	/**
