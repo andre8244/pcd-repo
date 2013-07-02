bca23a915118f423f201065075de4f9121a3d763
andre <andre@kubuntu>
2013-07-02 14:37:09 +0200

    *Commit Message*

--- DeterminantCalculatorWorker/src/messages/Messages.java
+++ DeterminantCalculatorWorker/src/messages/Messages.java
@@ -2,136 +2,140 @@ package messages;
 
 import java.io.Serializable;
 
+/**
+ * The messages used by every worker actor.
+ * 
+ */
 public class Messages {
-
+	
 	// TODO eliminare
 	public static class OneRow implements Serializable {
-
+		
 		private final double[] firstRow;
 		private final double[] row;
 		private final String reqId;
 		private final int rowNumber;
-
+		
 		public OneRow(String reqId, double[] firstRow, double[] row, int rowNumber) {
 			this.firstRow = firstRow;
 			this.row = row;
 			this.reqId = reqId;
 			this.rowNumber = rowNumber;
 		}
-
-		public int getRowNumber(){
+		
+		public int getRowNumber() {
 			return rowNumber;
 		}
-
-		public String getReqId(){
+		
+		public String getReqId() {
 			return reqId;
 		}
-
+		
 		public double[] getFirstRow() {
 			return firstRow;
 		}
-
+		
 		public double[] getRow() {
 			return row;
 		}
 	}
-
+	
 	// TODO eliminare
 	public static class OneRowResult implements Serializable {
-
+		
 		private final double[] row;
 		private final String reqId;
 		private final int rowNumber;
-
-		public OneRowResult(String reqId, double[]row, int rowNumber) {
+		
+		public OneRowResult(String reqId, double[] row, int rowNumber) {
 			this.row = row;
 			this.reqId = reqId;
 			this.rowNumber = rowNumber;
 		}
-
-		public int getRowNumber(){
+		
+		public int getRowNumber() {
 			return rowNumber;
 		}
-
-		public String getReqId(){
+		
+		public String getReqId() {
 			return reqId;
 		}
-
+		
 		public double[] getRow() {
 			return row;
 		}
 	}
-
-    public static class ManyRows implements Serializable {
-
+	
+	public static class ManyRows implements Serializable {
+		
 		private final double[] firstRow;
 		private final double[][] rows;
 		private final String reqId;
 		private final int rowNumber;
-
+		
 		public ManyRows(String reqId, double[] firstRow, double[][] rows, int rowNumber) {
 			this.firstRow = firstRow;
 			this.rows = rows;
 			this.reqId = reqId;
 			this.rowNumber = rowNumber;
 		}
-
-		public int getRowNumber(){
+		
+		public int getRowNumber() {
 			return rowNumber;
 		}
-
-		public String getReqId(){
+		
+		public String getReqId() {
 			return reqId;
 		}
-
+		
 		public double[] getFirstRow() {
 			return firstRow;
 		}
-
+		
 		public double[][] getRows() {
 			return rows;
 		}
 	}
-
-    public static class ManyRowsResult implements Serializable {
-
+	
+	public static class ManyRowsResult implements Serializable {
+		
 		private final double[][] rows;
 		private final String reqId;
 		private final int rowNumber;
-
+		
 		public ManyRowsResult(String reqId, double[][] rows, int rowNumber) {
 			this.rows = rows;
 			this.reqId = reqId;
 			this.rowNumber = rowNumber;
 		}
-
-		public int getRowNumber(){
+		
+		public int getRowNumber() {
 			return rowNumber;
 		}
-
-		public String getReqId(){
+		
+		public String getReqId() {
 			return reqId;
 		}
-
+		
 		public double[][] getRows() {
 			return rows;
 		}
 	}
-
+	
 	public static class Remove implements Serializable { // TODO da eliminare?
-
+	
 		public Remove() {
 		}
 	}
-
+	
 	public static class AddWorkerAck implements Serializable {
-
+		
 		public AddWorkerAck() {
 		}
 	}
-
+	
 	public static class RemoveWorkerAck implements Serializable {
-
+		
 		public RemoveWorkerAck() {
 		}
 	}
