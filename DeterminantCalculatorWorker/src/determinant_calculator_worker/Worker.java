3468cd9dc7337a13cdf84a0269d5ad8a328c4f1d
andre <andre@kubuntu>
2013-07-02 19:40:59 +0200

    *Commit Message*

--- DeterminantCalculatorWorker/src/determinant_calculator_worker/Worker.java
+++ DeterminantCalculatorWorker/src/determinant_calculator_worker/Worker.java
@@ -7,11 +7,12 @@ import akka.actor.UntypedActor;
 import akka.remote.RemoteActorRefProvider;
 // IMPORT DEL WEB SERVICE CLIENT:
 //import localhost_client.*;
-import marco_client.*;
+//import marco_client.*;
 //import marcoXP_client.*;
 //import andreaf_client.*;
 //import andreafWindows8dualCore_client.*;
 //import leardini_linux.*;
+import leardini_mac.*;
 
 public class Worker extends UntypedActor {
 
