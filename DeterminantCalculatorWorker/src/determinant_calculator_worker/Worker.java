9e7a3173f038b75d10fd11286bfa0668164fb134
andre <andre@kubuntu>
2013-07-02 16:49:24 +0200

    *Commit Message*

--- DeterminantCalculatorWorker/src/determinant_calculator_worker/Worker.java
+++ DeterminantCalculatorWorker/src/determinant_calculator_worker/Worker.java
@@ -6,8 +6,8 @@ import akka.actor.Address;
 import akka.actor.UntypedActor;
 import akka.remote.RemoteActorRefProvider;
 // IMPORT DEL WEB SERVICE CLIENT:
-import localhost_client.*;
-//import marco_client.*;
+//import localhost_client.*;
+import marco_client.*;
 //import marcoXP_client.*;
 //import andreaf_client.*;
 //import andreafWindows8dualCore_client.*;
