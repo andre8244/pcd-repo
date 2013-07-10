fd0d290470de5ad9341a96ea41c14767d018e5ae
Andrea Fortibuoni <andrea.fortibuoni@studio.unibo.it>
2013-07-11 00:21:47 +0200

    FINAL cleanings

--- DeterminantCalculatorUser/src/determinant_calculator_user/SynchThread.java
+++ DeterminantCalculatorUser/src/determinant_calculator_user/SynchThread.java
@@ -3,10 +3,7 @@ package determinant_calculator_user;
 // IMPORT DEL WEB SERVICE CLIENT:
 import localhost_client.*;
 //import marco_client.*;
-//import marcoXP_client.*;
 //import andreaf_client.*;
-//import andreafWindows8dualCore_client.*;
-//import leardini_linux.*;
 //import leardini_mac.*;
 
 public class SynchThread extends Thread {
